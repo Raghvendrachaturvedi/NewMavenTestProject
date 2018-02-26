aef005a9617a5c7158dbdcc5669a057c8ecfff6a
raghvendra <raghvendra.qa@gmail.com>
2018-02-26 20:17:24 +0530

    First Check in

---
+++ NewMavenTestProject/src/main/java/NewMavenTestProject/NewMavenTestProject/LoginClass.java
@@ -0,0 +1,10 @@
+package NewMavenTestProject.NewMavenTestProject;
+
+public class LoginClass {
+
+	public static void main(String[] args) {
+		System.out.println("You are in Login class");
+
+	}
+
+}
