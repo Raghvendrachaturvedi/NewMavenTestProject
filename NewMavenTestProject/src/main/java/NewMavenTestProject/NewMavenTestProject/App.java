eae6e2552c471d78baba4e0253d9d0b0b02804cc
raghvendra <raghvendra.qa@gmail.com>
2018-02-26 19:58:56 +0530

    New Maven Test Project First Commit

---
+++ NewMavenTestProject/src/main/java/NewMavenTestProject/NewMavenTestProject/App.java
@@ -0,0 +1,13 @@
+package NewMavenTestProject.NewMavenTestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
