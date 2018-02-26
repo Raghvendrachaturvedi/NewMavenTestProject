75f00d1666115b915ae8d1220c428748c1cd562b
raghvendra <raghvendra.qa@gmail.com>
2018-02-26 20:25:05 +0530

    2 new classes added

---
+++ NewMavenTestProject/src/test/java/Module1/Test1.java
@@ -0,0 +1,10 @@
+package Module1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+System.out.println("Your Are in Test 1");
+
+	}
+
+}
---
+++ NewMavenTestProject/src/test/java/Module2/Test2.java
@@ -0,0 +1,10 @@
+package Module2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("You are in Class Test2");
+	}
+
+}
